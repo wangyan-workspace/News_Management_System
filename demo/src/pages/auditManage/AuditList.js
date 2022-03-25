6fad59a83ea477a5b56d14564b6255c8d929910e
wangyan-workspace <2107014530@qq.com>
2022-03-25 21:34:47 +0800

    审核新闻功能基本实现

--- demo/src/pages/auditManage/AuditList.js
+++ demo/src/pages/auditManage/AuditList.js
@@ -1,9 +1,111 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Table, Button, Tag, notification } from 'antd';
+import axios from 'axios';
 
-export default function AuditList() {
+export default function AuditList(props) {
+    //存储用户自己提交的需要被审核的新闻
+    const [dataSource, setDataSource] = useState([]);
+    const { username } = JSON.parse(localStorage.getItem("token"));
+
+    //auditState_ne=0 审核状态不等于0 对应的状态是"审核中", "已通过", "未通过"
+    //publishState_lte=1 发布状态<=1 对应的状态是"未发布", "待发布"
+    // const auditList = ["未审核", "审核中", "已通过", "未通过"];
+    // const publishList = ["未发布", "待发布", "已上线", "已下线"];
+    useEffect(() => {
+        axios(`/news?author=${username}&auditState_ne=0&publishState_lte=1&_expand=category`).then(res => {
+            console.log("审批的列表", res.data);
+            setDataSource(res.data);
+        })
+    }, [username])
+
+    const columns = [
+        {
+            title: '新闻标题',
+            dataIndex: 'title',
+            render: (title, item) => {
+                return <a href={`#/news-manage/preview/${item.id}`}>{title}</a>
+            }
+        },
+        {
+            title: '作者',
+            dataIndex: 'author'
+        },
+        {
+            title: "新闻分类",
+            dataIndex: 'category',
+            render: (category) => {
+                return <div>{category.title}</div>
+            }
+        },
+        {
+            title: "审核状态",
+            dataIndex: 'auditState',
+            render: (auditState) => {
+                const colorList = ["", "orange", "green", "red"];
+                const auditList = ["草稿箱", "审核中", "已通过", "未通过"];
+                return <Tag color={colorList[auditState]}>{auditList[auditState]}</Tag>
+            }
+        },
+        {
+            title: "操作",
+            render: (item) => {
+                return <div>
+                    {
+                        item.auditState === 1 && <Button onClick={() => handleRecall(item)}>撤销</Button>
+                    }
+                    {
+                        item.auditState === 2 && <Button danger onClick={() => handlePublish(item)}>发布</Button>
+                    }
+                    {
+                        item.auditState === 3 && <Button type='primary' onClick={() => handleUpdate(item)}>更新</Button>
+                    }
+                </div>
+            }
+        }
+    ]
+    const handleRecall = (item) => {
+        setDataSource(dataSource.filter(data => data.id !== item.id));
+
+        axios.patch(`/news/${item.id}`, {
+            auditState: 0
+        }).then(res => {
+            notification.info({
+                message: `通知`,
+                description:
+                    `您可以到草稿箱中查看您的新闻`,
+                placement: "topRight"
+            });
+        })
+    }
+
+    const handleUpdate = (item) => {
+        props.history.push(`/news-manage/update/${item.id}`);
+    }
+
+    const handlePublish = (item) => {
+        axios.patch(`/news/${item.id}`, {
+            "publishState": 2
+        }).then(res => {
+            props.history.push('/publish-manage/published');
+
+            notification.info({
+                message: `通知`,
+                description:
+                    `您可以到【发布管理/已经发布】中查看您的新闻`,
+                placement: "topRight"
+            });
+        })
+    }
     return (
         <div>
-            AuditList
+            <Table
+                dataSource={dataSource}
+                columns={columns}
+                pagination={{
+                    pageSize: 5
+                }}
+                rowKey={item => item.id}
+            />
         </div>
     )
 }
--- demo/src/pages/newsManage/NewsPreview.js
+++ demo/src/pages/newsManage/NewsPreview.js
@@ -17,6 +17,7 @@ export default function NewsPreview(props) {
     const auditList = ["未审核", "审核中", "已通过", "未通过"];
     const publishList = ["未发布", "待发布", "已上线", "已下线"];
 
+    const colorList = ["black", "orange", "green", "red"];
 
     return (
         <div>
@@ -34,8 +35,8 @@ export default function NewsPreview(props) {
                                 newsInfo.publishTime ? moment(newsInfo.publishTime).format("YYYY/MM/DD HH:mm:ss") : "-"
                             }</Descriptions.Item>
                             <Descriptions.Item label="区域">{newsInfo.region}</Descriptions.Item>
-                            <Descriptions.Item label="审核状态" ><span style={{ color: "red" }}>{auditList[newsInfo.auditState]}</span></Descriptions.Item>
-                            <Descriptions.Item label="发布状态" ><span style={{ color: "red" }}>{publishList[newsInfo.publishState]}</span></Descriptions.Item>
+                            <Descriptions.Item label="审核状态" ><span style={{ color: colorList[newsInfo.auditState] }}>{auditList[newsInfo.auditState]}</span></Descriptions.Item>
+                            <Descriptions.Item label="发布状态" ><span style={{ color: colorList[newsInfo.auditState] }}>{publishList[newsInfo.publishState]}</span></Descriptions.Item>
                             <Descriptions.Item label="访问数量">{newsInfo.view}</Descriptions.Item>
                             <Descriptions.Item label="点赞数量">{newsInfo.star}</Descriptions.Item>
                             <Descriptions.Item label="评论数量">0</Descriptions.Item>
