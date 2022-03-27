d39f35cb90af9718c73514f957929ad9876cdddc
wangyan-workspace <2107014530@qq.com>
2022-03-27 21:06:20 +0800

    首页面功能实现

--- demo/src/pages/home/home.js
+++ demo/src/pages/home/home.js
@@ -1,65 +1,247 @@
-import React from 'react';
-import { Button } from 'antd';
-// import axios from 'axios';
+import React, { useEffect, useState, useRef } from 'react'
+import { Card, Col, Row, List, Avatar, Drawer } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import axios from 'axios'
+import * as Echarts from 'echarts'
+import _ from 'lodash'
 
+const { Meta } = Card;
+
+// import axios from 'axios'
 export default function Home() {
-  const ajax = () => {
-    //取数据  get 查
-    // axios.get("http://localhost:8000/posts").then(res => {
-    //   console.log(res.data);
-    // })
+  //存储已经发布的用户最常浏览的新闻
+  const [viewList, setViewList] = useState([]);
+  // 存储已经发布的最受欢迎的新闻
+  const [starList, setStarList] = useState([]);
+  //存储已经发布的所有新闻
+  const [allList, setAllList] = useState([]);
+  //饼状图是否展示出来
+  const [visible, setVisible] = useState(false);
+  //对应饼状图 防止多次初始化
+  const [pieChart, setPieChart] = useState(null);
 
-    //取某一条数据 get
-    //返回一个对象
-    // axios.get("http://localhost:8000/posts/2").then(res => {
-    //   console.log(res.data);
-    // })
+  //柱状图
+  const barRef = useRef();
+  //饼状图
+  const pieRef = useRef();
 
-    //返回一个数组，数组中包含着想要获取的对象
-    // axios.get("http://localhost:8000/posts?id=3").then(res => {
-    //   console.log(res.data);
-    // })
+  //获取已经发布的用户最常浏览的新闻
+  useEffect(() => {
+    axios.get("/news?publishState=2&_expand=category&_sort=view&_order=desc&_limit=6").then(res => {
+      // console.log(res.data)
+      setViewList(res.data);
+    })
+  }, [])
 
-    //增 post
-    // axios.post("http://localhost:8000/posts",{
-    //   title: "444",
-    //   author: "huahua"
-    // })
+  //获取已经发布的最受欢迎的新闻
+  useEffect(() => {
+    axios.get("/news?publishState=2&_expand=category&_sort=star&_order=desc&_limit=6").then(res => {
+      // console.log(res.data)
+      setStarList(res.data);
+    })
+  }, [])
 
-    //更新 put  覆盖式修改，不需要改动的属性如果没有一起写进数据里，将会被覆盖掉，操作危险
-    // axios.put("http://localhost:8000/posts/1",{
-    //   title: "11111111-修改"
-    // })
+  //获取已经发布的所有新闻
+  useEffect(() => {
+    axios.get("/news?publishState=2&_expand=category").then(res => {
+      // console.log(res.data)
 
-    //更新patch 补丁式更新 将需要修改的属性添加到对应属性的对象中
-    // axios.patch("http://localhost:8000/posts/1",{
-    //   title: "1111-修改-11111",
-    //   number: 0
-    // })
+      //lodash.groupBy 处理数组对象按需分组
+      renderBarView(_.groupBy(res.data, item => item.category.title));
 
-    //删除 delete  除了删除对应的这一条数据，还会将相关联的数据一起删除掉
-    // axios.delete("http://localhost:8000/posts/1")
+      setAllList(res.data);
+    })
 
-    //表关联
-    //_embed  向下关联  
-    //在posts接口中的每一个对象下添加comments的数据，comments中存储着关联的关系，
-    //两者之间的关系是通过posts下的对象id属性和comments下的对象postId属性相匹配确定的
-    // axios.get("http://localhost:8000/posts?_embed=comments").then(res => {
-    //   console.log(res.data);
-    // })
+    // 离开首页时，去掉onresize方法，防止影响到其他的页面
+    return () => {
+      window.onresize = null
+    }
+  }, [])
 
-    //_expand 向上关联
-    //在comments接口中的每一个对象下添加post的数据，post中存储着关联的关系，
-    //两者之间的关系是通过posts下的对象id属性和comments下的对象postId属性相匹配确定的
-    //虽然是关联posts这个接口，但是_expand=post还是要这么写，不能加s，固定要求
-    // axios.get("http://localhost:8000/comments?_expand=post").then(res => {
-    //   console.log(res.data);
-    // })
+  const renderBarView = (obj) => {
+    var myChart = Echarts.init(barRef.current);
+
+    // 指定图表的配置项和数据
+    var option = {
+      title: {
+        text: '新闻分类图示'
+      },
+      tooltip: {},
+      legend: {
+        data: ['数量']
+      },
+      //x轴
+      xAxis: {
+        data: Object.keys(obj),
+        axisLabel: {
+          rotate: "45",
+          interval: 0
+        }
+      },
+      // y轴
+      yAxis: {
+        minInterval: 1  //设置数量表示最小间距为1，防止1.5的情况
+      },
+      series: [{
+        name: '数量',
+        type: 'bar',
+        data: Object.values(obj).map(item => item.length)
+      }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+
+
+    //处理柱状图响应式变化，随着窗口的大小，改变柱状图的大小
+    window.onresize = () => {
+      // console.log("resize")
+      myChart.resize();
+    }
+  }
+
+  const renderPieView = (obj) => {
+    //数据处理工作
+    var currentList = allList.filter(item => item.author === username);
+    //lodash.groupBy 处理数组对象按需分组
+    var groupObj = _.groupBy(currentList, item => item.category.title);
+    var list = []
+    for (var i in groupObj) {
+      list.push({
+        name: i,
+        value: groupObj[i].length
+      })
+    }
+    var myChart;
+    //pieChart的作用防止渲染多次
+    if (!pieChart) {
+      myChart = Echarts.init(pieRef.current);
+      setPieChart(myChart)
+    } else {
+      myChart = pieChart
+    }
+    var option = {
+      title: {
+        text: '当前用户新闻分类图示',
+        // subtext: '纯属虚构',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left',
+      },
+      series: [
+        {
+          name: '发布数量',
+          type: 'pie',
+          radius: '50%',
+          data: list,
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    };
+
+    option && myChart.setOption(option);
 
   }
+
+  const { username, region, role: { roleName } } = JSON.parse(localStorage.getItem("token"))
   return (
-    <div>
-        <Button type="primary" onClick={ajax}>Button</Button>
+    <div className="site-card-wrapper">
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card title="用户最常浏览" bordered={true}>
+            <List
+              size="small"
+              // bordered
+              dataSource={viewList}
+              renderItem={item => <List.Item>
+                <a href={`#/news-manage/preview/${item.id}`}>{item.title}</a>
+              </List.Item>}
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="用户点赞最多" bordered={true}>
+            <List
+              size="small"
+              // bordered
+              dataSource={starList}
+              renderItem={item => <List.Item>
+                <a href={`#/news-manage/preview/${item.id}`}>{item.title}</a>
+              </List.Item>}
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            cover={
+              <img
+                alt="example"
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+            }
+            actions={[
+              <SettingOutlined key="setting" onClick={() => {
+                // 等到dom结构渲染才能渲染出饼状图，放在setTimeout里面处理
+                setTimeout(() => {
+                  setVisible(true)
+
+                  // init初始化
+                  renderPieView()
+                }, 0)
+              }} />,
+              <EditOutlined key="edit" />,
+              <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            <Meta
+              avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+              title={username}
+              description={
+                <div>
+                  <b>{region ? region : "全球"}</b>
+                  <span style={{
+                    paddingLeft: "30px"
+                  }}>{roleName}</span>
+                </div>
+              }
+            />
+          </Card>
+        </Col>
+      </Row>
+      <Drawer
+        width="500px"
+        title="个人新闻分类"
+        placement="right"
+        closable={true}
+        onClose={() => {
+          setVisible(false)
+        }}
+        visible={visible}
+      >
+        <div ref={pieRef} style={{
+          width: '100%',
+          height: "400px",
+          marginTop: "30px"
+        }}></div>
+      </Drawer>
+
+
+      <div ref={barRef} style={{
+        width: '100%',
+        height: "400px",
+        marginTop: "30px"
+      }}></div>
     </div>
-  );
+  )
 }
